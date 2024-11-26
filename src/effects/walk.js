9e7bc264c656c7fe49be0a69156801c62c3cf031
Thomas Musengwa <thomasmusengwa1@gmail.com>
2024-11-25 21:23:40 -0600

    :bug:

--- src/effects/walk.js
+++ src/effects/walk.js
@@ -1,21 +1,21 @@
 const { initializeLEDs } = require("../controllers/ledController");
 
-const walk = async (delay) => {
-  const { ws281x, rgb2hex, LEDS } = await initializeLEDs(true);
+const walk = async (delay = 200) => {
+  const { ws281x, colorwheel, LEDS } = await initializeLEDs(true);
 
   let offset = 0;
 
   function loop() {
     const pixels = new Uint32Array(LEDS);
     // Set the color at the current offset
-    pixels[offset] = rgb2hex(243, 210, 101);
+    pixels[offset % LEDS] = colorwheel(offset % 255);
 
     offset++;
 
     ws281x.render(pixels);
   }
 
-  setInterval(loop, delay || 250);
+  setInterval(loop, delay);
 };
 
 module.exports = { walk };
