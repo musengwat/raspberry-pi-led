8c18c821833ed343bd1914bbb528d441adc398df
Thomas Musengwa <thomasmusengwa1@gmail.com>
2024-11-25 20:01:31 -0600

    :bug: added colorwheel to import

--- src/effects/test.js
+++ src/effects/test.js
@@ -1,7 +1,7 @@
 const { initializeLEDs } = require("../controllers/ledController");
 
 const testLEDs = async (brightness) => {
-  const { ws281x, LEDS } = await initializeLEDs();
+  const { ws281x, colorwheel, LEDS } = await initializeLEDs();
   const pixels = new Uint32Array(LEDS);
   for (let i = 0; i < LEDS; i++) {
     pixels[i] = colorwheel((i * 256) / LEDS);
