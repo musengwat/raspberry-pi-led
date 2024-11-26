08f1a4b8c02bef775e458a660e4cad905cbdab88
Thomas Musengwa <thomasmusengwa1@gmail.com>
2024-11-25 20:03:15 -0600

    testing when leds are initalized

--- src/controllers/ledController.js
+++ src/controllers/ledController.js
@@ -2,13 +2,14 @@ const initializeLEDs = async () => {
   const { colorwheel, rgb2hex, StripType, ws281x } = await import("piixel");
 
   const LEDS = 200;
-
+  console.log(ws281x || "¯_(ツ)_/¯");
   // Configure the library. Must be called before calling `render`
   ws281x.configure({
     gpio: 18,
     leds: LEDS,
     type: StripType.WS2811_STRIP_GRB,
   });
+  console.log(ws281x || "bad");
 
   return { colorwheel, rgb2hex, StripType, ws281x, LEDS };
 };
