d53ee185c27b8229b382824a648ceca162a15283
Thomas Musengwa <thomasmusengwa1@gmail.com>
2024-12-07 01:48:27 -0600

    :bug: :broom: made calls actually async

--- src/routes/effectRoutes.js
+++ src/routes/effectRoutes.js
@@ -7,45 +7,68 @@ const { fill } = require("../effects/fill");
 
 const router = Router();
 // test LEDS
-router.post("/pulse", (req, res) => {
-  const { delay } = req.body;
-  pulse(delay);
-  res.json({ message: "LEDs updated" });
+router.post("/pulse", async (req, res) => {
+  try {
+    const { delay } = req.body;
+    const response = await pulse(delay);
+    res.json({ message: `LEDs updated ${response}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update LEDs", details: error.message });
+  }
 });
 
 //reset LEDS
-router.post("/rainbow", (req, res) => {
-  const { brightness } = req.body;
-  rainbow(brightness);
-  res.json({ message: "LEDs turned off" });
+router.post("/rainbow", async (req, res) => {
+  try {
+    const { brightness } = req.body;
+    const response = await rainbow(brightness);
+    res.json({ message: `LEDs updated ${response}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update LEDs", details: error.message });
+  }
 });
 
 // Sample GET route
 router.post("/walk", async (req, res) => {
-  const { delay } = req.body;
-  walk(delay);
-  res.json({ message: "Welcome to the API!" });
+  try {
+    const { delay } = req.body;
+    const response = await walk(delay);
+    res.json({ message: `LEDs updated ${response}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update LEDs", details: error.message });
+  }
 });
 
 // Sample POST route
-router.post("/fill", (req, res) => {
-  const { color, brightness } = req.body;
-  fill(color, brightness);
-  res.json({ color, status: "Data received" });
+router.post("/fill", async (req, res) => {
+  try {
+    const { color, brightness } = req.body;
+    const response = await fill(color, brightness);
+    res.json({ message: `LEDs updated ${response}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update LEDs", details: error.message });
+  }
 });
 
 // Sample POST route
-router.post("/flow", (req, res) => {
-  const { delay } = req.body;
-  flow(delay);
-  res.json({ status: "Data received" });
-});
-
-// Sample POST route
-router.post("/walk", (req, res) => {
-  const { delay } = req.body;
-  flow2(delay);
-  res.json({ status: "Data received" });
+router.post("/flow", async (req, res) => {
+  try {
+    const { delay } = req.body;
+    const response = await flow(delay);
+    res.json({ message: `LEDs updated ${response}` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Failed to update LEDs", details: error.message });
+  }
 });
 
 module.exports = router;
