657197eb67725beb611deccde62163c80d36223b
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-10 00:29:15 -0600

    :bug:

--- src/effects/fire.js
+++ src/effects/fire.js
@@ -2,16 +2,12 @@ const fire2 = async (ledContext, delay, brightness) => {
   const { ws281x, LEDS } = ledContext;
   const pixels = new Uint32Array(LEDS);
 
-  const fireColors = [0x7f3f00, 0x9f4f00, 0xbf5f00, 0xff7f00, 0xff9f00]; // Warm fire-like colors
-
+  const fireColors = [0x7f3f00, 0x9f4f00, 0xbf5f00, 0xff7f00, 0xff9f00];
   const loop = async () => {
-    // Create a fire simulation by shifting pixels' brightness and applying new random flickers
     for (let i = 0; i < LEDS; i++) {
-      // Apply random flickering effect by selecting a fire color at random
       const color = fireColors[Math.floor(Math.random() * fireColors.length)];
 
-      // Apply some flickering by slightly varying the color brightness
-      const flicker = Math.floor(Math.random() * 50); // Random flicker effect for variation
+      const flicker = Math.floor(Math.random() * 50);
       pixels[i] = color + flicker;
     }
 
@@ -20,8 +16,6 @@ const fire2 = async (ledContext, delay, brightness) => {
 
   setInterval(loop, delay);
 };
-
-module.exports = { fire };
 
 const fire = async (ledContext, delay, brightness) => {
   const { ws281x, LEDS } = ledContext;
