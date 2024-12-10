f4082d98f77d7c92372e463d1aea6eaf0ced1a0a
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-09 23:24:10 -0600

    :sparkles: added rainbow effect

--- src/effects/rainbow.js
+++ src/effects/rainbow.js
@@ -1,12 +1,17 @@
-const rainbow = async (ledContext, brightness) => {
+const rainbow = async (ledContext, delay, brightness) => {
   const { ws281x, colorwheel, LEDS } = ledContext;
   const pixels = new Uint32Array(LEDS);
-  for (let i = 0; i < LEDS; i++) {
-    pixels[i] = colorwheel((i * 256) / LEDS);
-  }
-  console.log(ws281x, "in test");
-  ws281x.render({ pixels, brightness: brightness || 0.4 });
-  return "leds have been turned on", LEDS;
+  let offset = 0;
+
+  const loop = async () => {
+    for (let i = 0; i < LEDS; i++) {
+      pixels[i] = colorwheel((i + offset) % 255);
+    }
+    offset = (offset + 1) % 255; // Wrap around after completing a cycle
+    await ws281x.render({ pixels, brightness: brightness || 0.8 });
+  };
+
+  return setInterval(loop, delay);
 };
 
 module.exports = { rainbow };
