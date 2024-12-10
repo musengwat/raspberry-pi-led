03e3fbc90a6e584566fa622354addfac2b275b9e
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-10 00:44:42 -0600

    :bug:

--- src/effects/waveformEffect.js
+++ src/effects/waveformEffect.js
@@ -6,8 +6,8 @@ const waveformEffect = async (ledContext, delay) => {
 
   const loop = async () => {
     for (let i = 0; i < LEDS; i++) {
-      let wave = Math.sin((i + offset) * 0.1);
-      let colorValue = Math.floor((wave + 1) * 127);
+      // let wave = Math.sin((i + offset) * 0.1);
+      let colorValue = Math.floor((i + offset) * 127);
       pixels[i] = colorwheel(colorValue);
       if (i === 0) {
         console.log(console.log(wave, colorValue));
