6b3f2807535a36373d33e5fecb4ac1d97b89f2b9
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-10 00:21:43 -0600

    :sparkles: added fade

--- src/effects/firework.js
+++ src/effects/firework.js
@@ -2,15 +2,30 @@ const firework = async (ledContext, delay, brightness, size) => {
   const { ws281x, colorwheel, LEDS } = ledContext;
   const pixels = new Uint32Array(LEDS);
 
+  const fadeDuration = 100;
+  let fadeDirection = 1;
+  let fadeValue = 0;
+
   const loop = async () => {
-    // Create bursts of random color and random positions
     const burstStart = Math.floor(Math.random() * LEDS);
-    const burstSize = Math.floor(Math.random() * 5) + size; // Random burst size
+    const burstSize = Math.floor(Math.random() * 5) + size;
     const color = Math.floor(Math.random() * 255);
+
+    fadeValue += fadeDirection * 0.05;
+    if (fadeValue >= 1 || fadeValue <= 0) {
+      fadeDirection *= -1;
+    }
+
+    pixels.fill(0);
 
     for (let i = 0; i < burstSize; i++) {
       const index = (burstStart + i) % LEDS;
-      pixels[index] = colorwheel(color);
+      const colorWithBrightness = colorwheel(color);
+      const fadedColor =
+        (colorWithBrightness & 0x00ffffff) |
+        (Math.floor(fadeValue * 255) << 24);
+
+      pixels[index] = fadedColor;
     }
 
     await ws281x.render({ pixels, brightness: brightness || 0.8 });
