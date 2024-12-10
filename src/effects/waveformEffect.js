da05b30ff9cbffcc14060d180d6ce4d170ced666
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-10 00:49:13 -0600

    :broom:

--- src/effects/waveformEffect.js
+++ src/effects/waveformEffect.js
@@ -6,9 +6,8 @@ const waveformEffect = async (ledContext, delay) => {
 
   const loop = async () => {
     for (let i = 0; i < LEDS; i++) {
-      // let wave = Math.sin((i + offset) * 0.1);
-      let wave = (i + offset) * 0.1;
-      let colorValue = Math.floor((wave + 1) * 255);
+      let wave = Math.sin((i + offset) * 0.1);
+      let colorValue = Math.floor((wave + 1) * 127);
       pixels[i] = colorwheel(colorValue);
     }
 
