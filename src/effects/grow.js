610a5b3282925fbccb961604a14e390ba7661682
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-09 22:41:31 -0600

    :back:

--- src/effects/grow.js
+++ src/effects/grow.js
@@ -2,20 +2,13 @@ const grow = async (ledContext, brightness) => {
   const { ws281x, colorwheel, LEDS } = ledContext;
   let offset = 0;
   const pixels = new Uint32Array(LEDS);
-  let direction = 1; // 1 for forward, -1 for backward
 
-  while (true) {
-    pixels.fill(0); // Clear the array before updating
+  for (let i = 0; i < LEDS; i++) {
+    offset++;
     pixels[offset % LEDS] = colorwheel(offset % 255);
+    console.log(offset, offset % 255);
 
     await ws281x.render({ pixels, brightness: brightness || 0.8 });
-
-    offset += direction;
-
-    if (offset >= LEDS || offset < 0) {
-      direction *= -1;
-      offset += direction;
-    }
   }
 };
 
