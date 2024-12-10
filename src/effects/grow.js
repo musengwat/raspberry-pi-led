1e786fb6cef31f46670bd0a3aded7d6d6d8c671e
Thomas Musengwa <thomasmusengwa@Thomass-MacBook-Air.local>
2024-12-09 22:34:52 -0600

    :sparkles: updated grow to shrink and loop

--- src/effects/grow.js
+++ src/effects/grow.js
@@ -2,13 +2,20 @@ const grow = async (ledContext, brightness) => {
   const { ws281x, colorwheel, LEDS } = ledContext;
   let offset = 0;
   const pixels = new Uint32Array(LEDS);
+  let direction = 1; // 1 for forward, -1 for backward
 
-  for (let i = 0; i < LEDS; i++) {
-    offset++;
+  while (true) {
+    pixels.fill(0); // Clear the array before updating
     pixels[offset % LEDS] = colorwheel(offset % 255);
-    console.log(offset, offset % 255);
 
     await ws281x.render({ pixels, brightness: brightness || 0.8 });
+
+    offset += direction;
+
+    if (offset >= LEDS || offset < 0) {
+      direction *= -1;
+      offset += direction;
+    }
   }
 };
 
